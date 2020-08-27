618cf9513e9f1d8a6265d18e7f7953d234aa94df
hss01248 <hss012489@gmail.com>
2020-08-27 19:42:04 +0800

    删除xx打印

--- buildSrc/src/main/java/cn/nekocode/jarfilter/JarFilterTransform.kt
+++ buildSrc/src/main/java/cn/nekocode/jarfilter/JarFilterTransform.kt
@@ -95,7 +95,6 @@ class JarFilterTransform(private val project: Project) : Transform() {
         if(jarInput.file.name.matches(Regex("\\d+.jar"))){
             //被其他transform搞过了,已经没有包名之类的信息了,需要注意的是,Utils.copyAndFilterJar在一次中只调用一次
             Utils.copyAndFilterJarIfNameNum(jarInput.file, outJarFile, jarFilters)
-            println("xx")
             return
         }
 
